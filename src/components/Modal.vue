93780368846c48317e183d6b85ce3429faa89e4d
chris <chris>
2023-03-16 19:22:13 +0000

    howto scoped and global css

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -6,7 +6,7 @@
     </div>
 </template>
 
-<style>
+<style scoped>
     .modal {
         width: 400px;
         padding: 20px;
@@ -20,5 +20,8 @@
         background: rgba(0,0,0,0.5);
         width: 100%;
         height: 100%;
+    }
+    .modal p {
+        font-style: normal;
     }
 </style>
